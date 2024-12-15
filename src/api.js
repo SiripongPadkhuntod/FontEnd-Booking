4b6ae3e2e1b8ebd5bd128fde9ed8673658a1fd93
Thyme <siripong.p64@rsu.ac.th>
2024-12-15 23:06:50 +0700

    Update API base URL to use HTTPS for improved security

--- src/api.js
+++ src/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-// export default axios.create({
-//   baseURL: `https://backend-6ug4.onrender.com/`
-// });
+export default axios.create({
+  baseURL: `https://backend-6ug4.onrender.com/`
+});
 
 //HTTP
-export default axios.create({
-  baseURL: `http://kazuyaserver.thddns.net:6867/`
-});
+// export default axios.create({
+//   baseURL: `http://kazuyaserver.thddns.net:6867/`
+// });
 
 //HTTPS
 // export default axios.create({
