e8b1875338535958c73be910d2d012d14340ff0b
Thyme <siripong.p64@rsu.ac.th>
2024-12-15 22:32:33 +0700

    Update API configuration to use HTTPS for improved security

--- src/api.js
+++ src/api.js
@@ -4,7 +4,12 @@ import axios from 'axios';
 //   baseURL: `https://backend-6ug4.onrender.com/`
 // });
 
+//HTTP
+// export default axios.create({
+//   baseURL: `http://kazuyaserver.thddns.net:6867/`
+// });
 
+//HTTPS
 export default axios.create({
   baseURL: `https://kazuyaserver.thddns.net:6867/`
 });
